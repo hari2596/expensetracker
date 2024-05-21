568270e71d0c34e61fbc768c8c22a7dfbc568c0c
hari2596 <>
2024-05-21 17:45:22 +0530

    16th  commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,62 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react';
 //styles
 import './App.css';
 //components
-import Navbar from './Navbar/Navbar'
-import AppHead from './Apphead/Apphead'
+import Navbar from './Navbar/Navbar';
+import AppHead from './Apphead/Apphead';
 import AppBody from './Appbody/Appbody';
 //contexts
-import { TransactionsContext, MoneyContext } from "./AllContexts"
+import { TransactionsContext, MoneyContext } from './AllContexts';
 //variables
 import { dummyData } from './dummyTranactions';
+
+const LOCAL_STORAGE_KEY = 'allData';
 
 function App() {
   const [money, setMoney] = useState({
     balance: 3800,
     expenses: 1200
-  })
+  });
   const [transactionData, setTransactionData] = useState(dummyData);
-  const initialRender = useRef(true);
+  const [isInitialRender, setIsInitialRender] = useState(true);
 
-  useEffect(()=>{
-    if(initialRender.current)  onLoad();
+  useEffect(() => {
+    if (isInitialRender) {
+      onLoad();
+      setIsInitialRender(false);
+    }
+  }, [isInitialRender]);
 
-    return(() => {
-      initialRender.current = false;
-    })
-  }, [])
+  const saveToLocalStorage = useCallback(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({ money, transactionData }));
+  }, [money, transactionData]);
 
-  useEffect(()=> {
-    //save data to local storage and if it is initial render skip saving
-    if(!initialRender.current) localStorage.setItem("allData", JSON.stringify({money, transactionData}));
-  }, [money, transactionData])
+  useEffect(() => {
+    if (!isInitialRender) {
+      saveToLocalStorage();
+    }
+  }, [money, transactionData, isInitialRender, saveToLocalStorage]);
 
-  //functions
   const onLoad = () => {
-    //load data from local storage if present
-    const localData = localStorage.getItem("allData");
-    if(localData){
-      const {money, transactionData} = JSON.parse(localData);
+    const localData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (localData) {
+      const { money, transactionData } = JSON.parse(localData);
       setMoney(money);
       setTransactionData(transactionData);
     }
-  }
-  
+  };
 
   return (
     <main className='App'>
       <MoneyContext.Provider value={[money, setMoney]}>
-      <TransactionsContext.Provider value={[transactionData, setTransactionData]}>
-        <Navbar />
-        <AppHead balance={money.balance} expenses={money.expenses}/>
-        <AppBody transactionData={transactionData}/>
-      </TransactionsContext.Provider> 
+        <TransactionsContext.Provider value={[transactionData, setTransactionData]}>
+          <Navbar />
+          <AppHead balance={money.balance} expenses={money.expenses} />
+          <AppBody transactionData={transactionData} />
+        </TransactionsContext.Provider>
       </MoneyContext.Provider>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
